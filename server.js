1fe6b7b9ea3181a3da16f7c67ad0614ebb1c2795
Samuelfournier <sam.fournier1212@gmail.com>
2021-10-22 17:34:28 -0400

    Test

--- server.js
+++ server.js
@@ -9,63 +9,63 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // var http = require('http').Server(app);
 // var io = require('socket.io')(http);
 
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+// const http = require('http');
+// const server = http.createServer(app);
+// const { Server } = require("socket.io");
+// const io = new Server(server);
 
-const port = process.env.PORT || 3000
+// const port = process.env.PORT || 3000
 
-app.use(express.static(__dirname));
+// app.use(express.static(__dirname));
 
-var mysql = require('mysql')
-var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'chat-room'
-})
+// var mysql = require('mysql')
+// var connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'chat-room'
+// })
 
 
-app.get('/messages', (req, res) => {
+// app.get('/messages', (req, res) => {
 
-    connection.query('SELECT * from message', function (err, rows, fields) {
-        if (err) throw err
+//     connection.query('SELECT * from message', function (err, rows, fields) {
+//         if (err) throw err
       
-        res.send(rows);
-    })
-})
+//         res.send(rows);
+//     })
+// })
   
 
-app.post('/messages', (req, res) => {
-    // var message = new Message(req.body);
+// app.post('/messages', (req, res) => {
+//     // var message = new Message(req.body);
 
-    console.log(req.body);
-    let sql = `INSERT INTO MESSAGE(member_id,content,name) VALUES(1,"${req.body.content}", "${req.body.name}")`
+//     console.log(req.body);
+//     let sql = `INSERT INTO MESSAGE(member_id,content,name) VALUES(1,"${req.body.content}", "${req.body.name}")`
 
 
-    connection.query(sql, function (err, rows, fields) {
-        if (err) throw err
+//     connection.query(sql, function (err, rows, fields) {
+//         if (err) throw err
       
-        io.emit('message', req.body);
-        res.sendStatus(200);
-    })
-  })
+//         io.emit('message', req.body);
+//         res.sendStatus(200);
+//     })
+//   })
 
 
   
-io.on('connection', (socket) => {
-    console.log('a user is connected')
-})
+// io.on('connection', (socket) => {
+//     console.log('a user is connected')
+// })
 
-connection.connect((err) => {
-    if (err) throw err;
-    console.log('Connected!');
-  });
+// connection.connect((err) => {
+//     if (err) throw err;
+//     console.log('Connected!');
+//   });
 
-server.listen(port, () => {
-    console.log('server is running on port', server.address().port);
-});
+// server.listen(port, () => {
+//     console.log('server is running on port', server.address().port);
+// });
    
 
 // connection.end()
